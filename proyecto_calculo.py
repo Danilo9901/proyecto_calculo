b8092ea075e9d4a85fb637702d7de1ab9cfffa31
Edgard Decena <edecena@gmail.com>
2019-10-22 07:36:46 -0400

    Correción de funciones de cadena.

--- proyecto_calculo.py
+++ proyecto_calculo.py
@@ -10,98 +10,98 @@ https://github.com/ejdecena/calculo_numerico/codigos/generar_proyecto_pr.py
 
 def Alejandro_27525819():
     """
-    Recibe como parámetro los valores a, b, c y retorna el mínimo.
+    Recibe como parámetro una lista y retorna el promedio de la lista.
     """
     pass
 
 
 def Anibal_26887484():
     """
-    Recibe como parámetro una lista y retorna la lista invertida.
+    Recibe como parámetro una cadena y retorna una lista con las vocales de la cadena.
     """
     pass
 
 
 def Ashly_27424492():
     """
-    Recibe como parámetro una lista y retorna el promedio de la lista.
+    Recibe como parámetro un diccionario y retorna una lista con los valores del diccionario.
     """
     pass
 
 
 def Danilo_27424264():
     """
-    Recibe como parámetro una cadena y retorna la cadena invertida.
+    Recibe como parámetro los valores a, b, c y retorna el mínimo.
     """
     pass
 
 
 def Diego_27650623():
     """
-    Recibe como parámetro un diccionario y retorna los valores del diccionario.
+    Recibe como parámetro un diccionario y retorna una lista con las claves del diccionario.
     """
     pass
 
 
 def Enrique_27000054():
     """
-    Recibe como parámetro a, b, c y retorma el máximo.
+    Recibe como parámetro un diccionario y retorna el mínimo valor del diccionario.
     """
     pass
 
 
 def Estefano_26778542():
     """
-    Recibe como parámetro una cadena y retorma una lista con las consonantes de la cadena.
+    Recibe como parámetro una lista y retorna el máximo valor de la lista.
     """
     pass
 
 
 def Hector_25967387():
     """
-    Recibe como paŕametro una cadena y retorna el número de caracteres de la cadena.
+    Recibe como parámetro una lista y retorna la lista invertida.
     """
     pass
 
 
 def Jesus_22998438():
     """
-    Recibe como parámetro una lista y retorna el mínimo valor de la lista.
+    Recibe como parámetro una cadena y retorma una lista con las consonantes de la cadena.
     """
     pass
 
 
 def Jose_27525799():
     """
-    Recibe como parámetro un diccionario y retorna el mínimo valor del diccionario.
+    Recibe como parámetro un diccionario y retorna el máximo valor del diccionario.
     """
     pass
 
 
 def Mauricio_27202233():
     """
-    Recibe como parámetro una cadena y retorna una lista con las vocales de la cadena.
+    Recibe como parámetro una cadena y retorna la cadena invertida.
     """
     pass
 
 
 def Miguel_26842695():
     """
-    Recibe como parámetro un diccionario y retorna el máximo valor del diccionario.
+    Recibe como parámetro una lista y retorna el mínimo valor de la lista.
     """
     pass
 
 
 def Norlie_26707950():
     """
-    Recibe como parámetro una lista y retorna el máximo valor de la lista.
+    Recibe como parámetro a, b, c y retorma el máximo.
     """
     pass
 
 
 def Wuilmer_26625191():
     """
-    Recibe como parámetro un diccionario y retorna las claves del diccionario.
+    Recibe como paŕametro una cadena y retorna el número de caracteres de la cadena.
     """
     pass
 
